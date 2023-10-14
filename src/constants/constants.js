98a297698c00f0ae5071ce84c337822f4ef15575
olayide-paystack <106555099+olayide-paystack@users.noreply.github.com>
2023-10-14 11:22:38 +0100

    Add constants

---
+++ src/constants/constants.js
@@ -0,0 +1,25 @@
+export class Constants {
+    weight_unit = {
+        KG: 'kg'
+    }
+
+    size_unit = {
+        CM: 'cm'
+    }
+
+    currency = {
+        NGN: 'NGN',
+        USD: 'USD'
+    }
+
+    parcel_item_type = {
+        PARCEL: 'parcel',
+        DOC: 'document'
+    }
+
+    packaging_type = {
+        BOX: 'box',
+        ENVELOPE: 'envelope',
+        SOFT_PACK: 'soft-packaging'
+    }
+}
